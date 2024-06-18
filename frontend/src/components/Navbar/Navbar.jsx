7d92baa9e1382b88f32a9b78a94e446982bee32c
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-06-18 14:07:30 -0400

    feat: prepare Navbar component

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return(
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <p className="text-light">This is a navbar</p>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
