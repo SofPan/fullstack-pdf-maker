1654b1fefe06958bc3f21b4317a5d88090ccccf1
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-06-18 15:11:00 -0400

    setup: create document controller

---
+++ app/controllers/document.controller.js
@@ -0,0 +1,29 @@
+const db = require("../models");
+const Document = db.documents;
+const Op = db.Sequelize.Op;
+
+// Create a new document
+
+exports.create = (req, res) => {
+  console.log("create a new document");
+};
+
+// Find all documents
+exports.findAll = (req, res) => {
+
+}
+
+// Retrieve one document by id
+exports.findOne = (req, res) => {
+
+};
+
+// Update a Document by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a Document by id in the request
+exports.delete = (req, res) => {
+
+};
