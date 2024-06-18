8ecb0f128767eb873db99759182ebe129f1bece8
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-06-18 13:56:13 -0400

    setup: prepare User controller

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,23 @@
+const db = require("../models");
+const User = db.users;
+const Op = db.Sequelize.Op;
+
+// Create a new user
+exports.create = (req, res) => {
+
+};
+
+// Retrieve a user by id
+exports.findOne = (req, res) => {
+
+};
+
+// Update a User by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a User with the specified id in the request
+exports.delete = (req, res) => {
+
+};
