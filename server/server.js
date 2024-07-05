83dd713758e9bd7f95a54c72862d1990a2a6405e
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-07-05 11:44:48 -0400

    test database connected to API

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const express = require('express');
+const pool = require(__dirname + "/db/connection.js");
 
 const morgan = require('morgan');
 
@@ -12,9 +13,21 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+const getUser = (req, res) => {
+  pool.query('SELECT * FROM users', (error, users) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(users.rows)
+  })
+};
+
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.get('/users', getUser);
 
 app.listen(PORT, () => {
   console.log(`Server listening on the port  ${PORT}`);
