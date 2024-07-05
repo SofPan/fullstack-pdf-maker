093abcf551d848a467cfed2d18017f71f7f1d997
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-07-05 12:07:01 -0400

    test axios request on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('/users')
+      .then(res => res.data)
+      .then(data => setData(data))
+      .catch(err => console.error('Error fetching users', err));
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <span>User Info</span>
+        {data.map(user => <p key={user.id}>{user.email}</p>)}
+      </div>
     </div>
   );
 }
