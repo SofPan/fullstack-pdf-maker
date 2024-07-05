96fc308b65548e4c83f8829b0d2c92ad1523984c
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-07-05 11:33:54 -0400

    install morgan

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const express = require('express');
 
+const morgan = require('morgan');
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
